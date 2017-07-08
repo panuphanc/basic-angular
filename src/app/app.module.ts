7783995f5184cc7d9eb66267cdd3e431146d4590
panuphanc <nongmee8@gmail.com>
2017-07-08 16:34:35 +0700

    create repo

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { WarningComponent } from './warning/warning.component';
+import { ServersComponent } from './servers/servers.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    WarningComponent,
+    ServersComponent,
+    NavComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
